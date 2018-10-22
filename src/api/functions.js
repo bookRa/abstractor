5ccd7a69eaaf7c01c697148a6ba77813f63601d0
Omar Abdelbadie <38739863+bookRa@users.noreply.github.com>
2018-10-22 08:15:38 -0700

    updated flask url to pythonAnywhere

--- src/api/functions.js
+++ src/api/functions.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-let FLASK_URL = "http://127.0.0.1:5000"
+let FLASK_URL = "http://bookra.pythonanywhere.com/"
 
 const sampleReturn = {
     colorizedText: "I'm returning from axios...sych!",
